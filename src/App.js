dc07b1e00efb3db9d594b86ad951ccb1f4a2b198
Santhil Kalantre <santhil@Santhils-MacBook-Air.local>
2023-05-01 00:44:40 +0530

    Yet to add dockerignore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './component/GamerLogin';
+import Footer from './components/footer';
+import { BrowserRouter as Router, Routes, Route, Form } from 'react-router-dom';
+import GamerLoginPage from './components/GamerLogin';
+import AdminLoginPage from './components/AdminLogin';
+import HomePage from './components/Landingpage';
+import GamerRegisterPage from './components/GamerRegister';
+import AdminHome from './components/AdminHome';
+import Navbar from './components/navbar';
+import AdminGamerDetails from './components/adminGamerDetails';
+import AddCategory from './components/adminAddcategory';
+import AdminAddGame from './components/adminAddGame';
+import GamerHome from './components/gamerHome';
+import AdminAddBill from './components/adminGenerateBill';
+import FormWala from './components/formWala';
+import AdminSeeBill from './components/adminSeeBill';
+import AdminViewByGid from './components/adminviewBillsByGid';
+import ViewBill from './components/viewbill';
+import GamerViewBill from './components/gamerSeeBill';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Navbar/>
+      <Router>
+        <Routes>
+        <Route exact path="/" element={<HomePage/>}/>
+        <Route path="/admin/login" element={<AdminLoginPage/>}/>
+        <Route path="/admin/home" element={<AdminHome/>}/>
+        <Route path="/gamer/login" element={<GamerLoginPage/>}/>
+        <Route path="/gamer/register" element={<GamerRegisterPage/>}/>
+        <Route path="/admin/gamer/:id" element={<AdminGamerDetails/>}/>
+        <Route path="/admin/addcat" element={<AddCategory />}/>
+        <Route path="/admin/addgame" element={<AdminAddGame/>}/>
+        <Route path="/gamer/home" element={< GamerHome/>}/>
+        <Route path="/admin/generatebill" element={< AdminAddBill/>} />
+        <Route path="/admin/seebill" element={< AdminSeeBill/>} />
+        <Route path="/admin/view/:id" element={< AdminViewByGid/>} />
+        <Route path="/admin/bill/:billid/:gamerid" element={< ViewBill/>} />
+        <Route path="/gamer/seebill" element={< GamerViewBill/>} />
+        </Routes>
+        
+      </Router>
+      <Footer/>
     </div>
   );
 }
