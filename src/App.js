10645c27baa359cb33791176dbb980308790d681
Santhil Kalantre <santhil@Santhils-MacBook-Air.local>
2023-05-07 14:37:16 +0530

    Webhook 5

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import GamerViewBill from './components/gamerSeeBill';
 function App() {
   return (
     <div className="App">
-      
+      {/* ads */}
       <Navbar/>
       <Router>
         <Routes>
